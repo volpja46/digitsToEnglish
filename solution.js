472c51a92c639218472de597c6a3ee8f1017ed58
Jen Volpe <jvolpe721@gmail.com>
2018-01-29 16:10:41 -0500

    code to convert digits to actual words

--- solution.js
+++ solution.js
@@ -1,5 +1,6 @@
 // Task: turn numeric digits into english words. For example, input (9410193) & RESULT = "ninety-four million one hundred one thousand ninety-three"
 
+//  chunks from 0-999- @ most will be 3 digits
 function breakDownNumber (num){
   var num = 9410193,
   thousands = [];
@@ -25,3 +26,30 @@ const tens = [
 ];
 
 const scales = ["thousand", "million", "billion", "trillion"];
+
+// 3
+
+
+function isNotNull (item) {
+  return !!item;
+  // double bang operator "!!"
+  // forces true or false value!
+}
+
+// translate a num from 1-999 into English
+function toEnglish(num) {
+  var thousands, hundredsPlace, tensPlace, onesPlace, words = [];
+
+  if(num < 20) {
+    return ones[num - 1];
+  }
+
+  if(num < 100) {
+    onesPlace = num % 10;
+    tensPlace = num / 10 | 0; // equivalent to Math.floor(num / 10)
+
+    words.push(tens[tensPlace - 1]);
+    words.push(toEnglish(onesPlace));
+
+    return words.filter(isNotNull).join("-");
+  }
